7eb36261a658bea82c68760cfa4440c4e79f320a
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-07 02:12:48 +0000

    👩‍💼

---
+++ s/gety.fish
@@ -0,0 +1,5 @@
+#!/bin/fish 
+wget https://gitlab.com/sniffowomo/sn-dgn2/-/raw/main/sn/g.fish
+wget https://gitlab.com/sniffowomo/sn-dgn2/-/raw/main/sn/in.fish
+wget https://gitlab.com/sniffowomo/sn-dgn2/-/raw/main/sn/div.txt
+wget https://gitlab.com/sniffowomo/sn-dgn2/-/raw/main/sn/pnp.fish 
