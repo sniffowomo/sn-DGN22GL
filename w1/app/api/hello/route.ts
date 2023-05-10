241c94a83bc54c0395710be616d4efa3ca9f90c5
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-10 17:53:58 +0530

    Added with session cookies

--- w1/app/api/hello/route.ts
+++ w1/app/api/hello/route.ts
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import { cookies } from "next/headers"
+
+export async function GET(request: Request) {
+  const cookieStore = cookies()
+  const token = cookieStore.get("token")
+
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: { "Set-Cookie": `token=${token}` },
+  })
 }
