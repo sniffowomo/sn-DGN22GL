6164ab9a95c740d922dd9fb9398fb75f89bc263f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-10 02:41:44 +0000

    👩‍💼

--- w1/app/page.tsx
+++ w1/app/page.tsx
@@ -3,15 +3,12 @@ import E2 from "./components/desc"
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-1 gap-4">
-      <h1 className="text-justify text-5xl text-fuchsia-700">
-        {" "}
-        Whatup Ma Homie{" "}
-      </h1>
+    <main className="grid grid-cols-1 gap-4 text-center">
+      <h1 className="text-5xl text-fuchsia-700">Whatup Ma Homie</h1>
       <h2> YoHoHo </h2>
-      <p className="flex">
+      <p className="bg-amber-800 font-extrabold">
         This is going to be just a test of shit <br /> And yet we will see what
-        is happening
+        is happening <br /> This project is for testing out the
       </p>
       <E2 />
     </main>
