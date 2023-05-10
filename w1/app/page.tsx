662d42a300fbe9a0cd3d8c5a0891bc256f6f0be6
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-10 02:33:29 +0000

    👩‍💼

--- w1/app/page.tsx
+++ w1/app/page.tsx
@@ -3,10 +3,16 @@ import E2 from "./components/desc"
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 gap-4">
-      <h1 className="text-justify text-5xl"> Whatup </h1>
+    <main className="grid grid-cols-1 gap-4">
+      <h1 className="text-justify text-5xl text-fuchsia-700">
+        {" "}
+        Whatup Ma Homie{" "}
+      </h1>
       <h2> YoHoHo </h2>
-      <p>This is going to be just a test of shit</p>
+      <p className="flex">
+        This is going to be just a test of shit <br /> And yet we will see what
+        is happening
+      </p>
       <E2 />
     </main>
   )
