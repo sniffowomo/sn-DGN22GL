12fdf107d6d7871366ef728a711c344b0426cba2
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-11 14:23:30 +0000

    👩‍💼

--- w2/app/api/todos/[Id]/route.ts
+++ w2/app/api/todos/[Id]/route.ts
@@ -2,8 +2,18 @@ import { NextResponse } from "next/server"
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
 
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf(`/`) + 1)
+type Props = {
+  params: {
+    id: string
+  }
+}
+
+export async function GET(request: Request, { params: { id } }: Props) {
+  // const id = request.url.slice(request.url.lastIndexOf(`/`) + 1) by author
+  /*
+  Note This was mentioned in the vid , that the above method with the type props is the next js way of getting the id not e
+  the export async function GET(new additions here to unerstand)
+  */
 
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`)
 
