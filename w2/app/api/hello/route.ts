3c8a7041fd168665ac22c252f068d010cc8612ad
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-05-11 19:30:44 +0000

    👩‍💼

--- w2/app/api/hello/route.ts
+++ w2/app/api/hello/route.ts
@@ -1,8 +1,13 @@
 import { cookies } from "next/headers"
+import { limiter } from "../config/limiter"
 
 export async function GET(request: Request) {
   const cookieStore = cookies()
   const token = cookieStore.get("token")
+
+  // For the limiter
+  const remaining = await limiter.removeTokens(1)
+  console.log("remaining: ", remaining)
 
   return new Response("Hello, Next.js!", {
     status: 200,
